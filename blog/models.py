338573063d6ba87b63ab88ad7335bd6f61709101
Deniz Gülbaharlı <artismaymunlar@gmail.com>
2016-02-13 17:21:00 +0200

    extensions

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,24 @@ class Post(models.Model):
             blank=True, null=True)
 
     def yayinla(self):
-        self. yayinlama_tarihi = timezone.now()
+        self. yayinlanma_tarihi = timezone.now()
         self.save()
 
     def __str__(self):
         return self.baslik
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    yazar = models.CharField(max_length=200)
+    yazi = models.TextField()
+    yaratilis_tarihi = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.yazi
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'', include('blog.urls')),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
